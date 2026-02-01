67981bcfd5de5710e2525855d424e3d2e09f39a7
Ethan Stark <19176630+EthanJStark@users.noreply.github.com>
2026-02-01 11:42:09 -0500

    feat: export episode workflow functions from library

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,28 @@ export { MoltbookClient } from './api/client.js';
 export { scrapeDaily } from './scraper/daily.js';
 export { generateReport } from './report/markdown.js';
 export * from './schema/moltbook.js';
+
+// Episode workflow exports
+export {
+  createEpisode,
+  confirmEpisode,
+  deleteEpisode,
+  transcribeEpisode,
+  publishEpisode,
+  getEpisodeStatus,
+  getAllEpisodesStatus,
+} from './episode/index.js';
+
+export type {
+  CreateEpisodeOptions,
+  CreateEpisodeResult,
+  ConfirmEpisodeOptions,
+  DeleteEpisodeOptions,
+  TranscribeEpisodeOptions,
+  TranscribeResult,
+  PublishEpisodeOptions,
+  EpisodeStatusInfo,
+  EpisodeMetadata,
+  CoveredPosts,
+  EpisodeStatus,
+} from './episode/index.js';
